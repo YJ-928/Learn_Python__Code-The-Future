c280a597664fca63c116acd1f86a51109cce662c
YJ-928 <ymbj28112000@gmail.com>
2022-11-23 18:27:33 +0530

    Update

--- Simple-Projects/Random Password Gen.py
+++
@@ -1,7 +0,0 @@
-import random
-# Max lenght of paasword is 75
-passlen = int(input("enter the length of password:"))
-s ="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-p =  "".join(random.sample(s,passlen ))
-print ("Your Password is :\n")
-print (p)
--- Simple-Projects/Simple-Pyramid.py
+++
@@ -1,9 +0,0 @@
-def pypart(n):
-    for i in range (0,n):
-        for j in range (0,i+1):
-            print("*",end=" ")
-        print("\r")
-
-print("Enter the size of pyramid:\n")
-n = int(input("Enter the value for size of pyramid:\n"))
-pypart(n)
